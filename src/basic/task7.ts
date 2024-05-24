66836d6cc21fc994c9848a759df5cf226671a19a
Hatori <ahgangadm@gmail.com>
2024-05-24 20:29:23 +0300

    Create task 7

---
+++ src/basic/task7.ts
@@ -0,0 +1,30 @@
+// Task 7
+
+type Post = {
+  title: string;
+  likes: number;
+  accounts: Array<string>;
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1: Post = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2: Post = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
